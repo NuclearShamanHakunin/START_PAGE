1e09600faa96fd8464188912e89dec3bf0baa0e9
Marko Jovanovic <marko.jov101@gmail.com>
2020-11-15 18:20:43 +0100

    accordion

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import BookmarkAccordion from './Components/BookmarkAccordion'
+
 function App() {
   return (
     <div className="App">
+      <BookmarkAccordion />
     </div>
   );
 }
